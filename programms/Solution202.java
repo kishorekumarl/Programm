ada1cdd2446c83589176c55f4d08074757bc2f49
kishorekumarl <l.kishorekumar@gmail.com>
2026-06-08 13:02:13 -0700

    moved the getNext method

--- programms/Solution202.java
+++ programms/Solution202.java
@@ -1,17 +1,17 @@
 package programms;
 
-class Solution {
-    
-    public boolean isHappy(int n) {
-       private int getNext(int n) {
-            int totalSum = 0;
-            while (n != 0) {
-                int d = n % 10;
-                n = n / 10;
-                totalSum += d * d;
-            }
-            return totalSum;
+class Solution202 {
+     private int getNext(int n) {
+        int totalSum = 0;
+        while (n != 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
         }
+        return totalSum;
+    }
+    public boolean isHappy(int n) {
+
         if (n == 1) return true;
         int slow = n;
         int fast = n;
